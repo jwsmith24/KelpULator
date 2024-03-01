78f9a1bbdf234dd5fbe055b6059f473eecc22088
Jake <jsmith181997@gmail.com>
2024-03-01 09:51:31 -1000

    Add basic operations

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
 
 
 // Operations
+// All operations take 2 numbers as an argument
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+
+    if (b !== 0) return a / b;
+
+    alert("I can't believe you tried to divide by zero...");
+    return undefined;
+}
+
+
+function performOperation(operator, a, b) {
+
+    let result;
+
+    switch (operator) {
+        case 'add':
+            result = add(a, b);
+            break;
+        case 'subtract':
+            result = subtract(a, b);
+            break;
+        case 'multiply':
+            result = multiply(a, b);
+            break;
+        case 'divide':
+            result = divide(a, b);
+            break;
+        default:
+            alert("Operation not supported");
+            result = 0;
+    }
+
+
+
+
+}
