a0ab7625f7bae42a21f62c149260fc86ab017fd5
Jake <jsmith181997@gmail.com>
2024-03-03 09:41:40 -0500

    Add debug message for testing in remote browser

--- script.js
+++ script.js
@@ -207,6 +207,7 @@ const funFacts = [
 window.addEventListener('keydown', handleKeyBoardInput);
 
 buttons.forEach(button => button.addEventListener('click', () => {
+    console.log("Hi Ashleigh: " + button.id);
     handleInput(button.id)
 })
 );
